1a6cc6937ef1811191d0a4e929c7cec8efee1a0f
patelnaimishkumar <patel.naimishkumar@gmail.com>
2019-01-31 20:20:40 +0530

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule,ROUTES, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { EmployeedetailComponent } from './employeedetail/employeedetail.component';
+import { CreateemployeeComponent } from './createemployee/createemployee.component';
+
+const routes:Routes=[
+  {path:'list',component:EmployeedetailComponent},
+  {path:'create',component:CreateemployeeComponent}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeedetailComponent,
+    CreateemployeeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
